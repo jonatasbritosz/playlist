4135044f49b23a42533b58ecfc29a6e3e7bfdaf1
JonatasBrito <jonatasbrito.sousa@gmail.com>
2020-05-02 20:35:08 -0300

    Update PlaylistService.js
    Corrigido consulta do serviço

--- src/services/PlaylistService.js
+++ src/services/PlaylistService.js
@@ -4,13 +4,13 @@ export default class PlaylistService {
 
     getPlaylistByCity(city) {
 
-        return axios.get("http://192.168.0.15:8080/playlist?city=" + city).then(res => res.data);
+        return axios.get("http://localhost:8080/playlist?city=" + city).then(res => res.data);
 
     }
 
     getPlaylistByCoordinates(lat, lon) {
 
-        return axios.get("http://192.168.0.15:8080/playlist?lat=" + lat + "&lon=" + lon).then(res => res.data);
+        return axios.get("http://localhost:8080/playlist?lat=" + lat + "&lon=" + lon).then(res => res.data);
 
     }
 
